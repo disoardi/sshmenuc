b1ed32f12e0d2d7e70e02e83f2add4857144fde2
davide isoardi <davide.isoardi@exage.com>
2020-12-18 23:00:26 +0100

    fix setup.py readme.md file location

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 with open('requirements.txt') as requirements_file:
     required = requirements_file.read().splitlines()
 
@@ -8,7 +11,8 @@ setup(
     version='0.0.4',
     license='MIT',
     description='Command line SSH menu and helper utility with cluster support',
-    long_description=open('README.md').read(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Davide Isoardi',
     author_email='davide@isoardi.info',
     url='https://github.com/disoardi/sshmenu',
--- sshmenu/__main__.py
+++
@@ -1,4 +0,0 @@
-"""Called when launched with python -m sshmenu"""
-
-from .sshmenu import main
-main()
